de7c09fad62151fdb5df5c3551aede6f1fa5840d
Marcus Brown <marcus.brown1234567@gmail.com>
2023-11-25 21:41:24 -0500

    updated for A5

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,51 +1,185 @@
-const setData = require('../data/setData');
-const themeData = require('../data/themeData');
+require('dotenv').config();
+const Sequelize = require('sequelize');
 
-let sets = [];
+const sequelize = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+  }
+);
+const Theme = sequelize.define(
+  'Theme',
+  {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    name: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+const Set = sequelize.define(
+  'Set',
+  {
+    set_num: { type: Sequelize.STRING, primaryKey: true },
+    name: Sequelize.STRING,
+    year: Sequelize.INTEGER,
+    num_parts: Sequelize.INTEGER,
+    theme_id: Sequelize.INTEGER,
+    img_url: Sequelize.STRING,
+  },
+  {
+    createdAt: false, // disable createdAt
+    updatedAt: false, // disable updatedAt
+  }
+);
+Set.belongsTo(Theme, { foreignKey: 'theme_id' });
+
 const initialize = () => {
   return new Promise((res, rej) => {
     try {
-      setData.forEach((set) => {
-        const newTheme = themeData.find((data) => set.theme_id === data.id);
-        set.theme = newTheme.name;
-        sets.push({ ...set });
+      sequelize.sync().then(() => {
+        res();
       });
-      res();
-    } catch {
-      rej('unable to initialize sets array');
+    } catch (err) {
+      rej(err);
     }
   });
 };
 const getAllSets = () => {
   return new Promise((res, rej) => {
-    if (sets.length > 0) {
-      res(sets);
-    } else {
-      rej('no sets found');
-    }
+    sequelize.sync().then(() => {
+      Set.findAll({
+        include: [Theme],
+      })
+        .then((sets) => {
+          res(sets);
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
   });
 };
-
 const getSetByNum = (setNum) => {
   return new Promise((res, rej) => {
-    const setByNum = sets.filter((set) => set.set_num == setNum);
-    if (setByNum.length > 0) {
-      res(setByNum);
-    } else {
-      rej('no matching set nums');
-    }
+    sequelize.sync().then(() => {
+      Set.findAll({
+        where: {
+          set_num: setNum,
+        },
+        include: [Theme],
+      })
+        .then((set) => {
+          res(set[0]);
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
   });
 };
 const getSetsByTheme = (theme) => {
   return new Promise((res, rej) => {
-    const setByTheme = sets.filter((set) =>
-      set.theme.toUpperCase().includes(theme.toUpperCase())
-    );
-    if (setByTheme.length > 0) {
-      res(setByTheme);
-    } else {
-      rej('no matching themes');
-    }
+    sequelize.sync().then(() => {
+      Set.findAll({
+        include: [Theme],
+        where: {
+          '$Theme.name$': {
+            [Sequelize.Op.iLike]: `%${theme}%`,
+          },
+        },
+      })
+        .then((set) => {
+          res(set);
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
+  });
+};
+const getAllThemes = () => {
+  return new Promise((res, rej) => {
+    sequelize.sync().then(() => {
+      Theme.findAll()
+        .then((themes) => {
+          res(themes);
+        })
+        .catch((err) => {
+          rej(err);
+        });
+    });
+  });
+};
+const addSet = (setData) => {
+  return new Promise((res, rej) => {
+    sequelize.sync().then(() => {
+      Set.create({
+        set_num: setData.set_num,
+        name: setData.name,
+        year: setData.year,
+        num_parts: setData.num_parts,
+        theme_id: setData.theme_id,
+        img_url: setData.img_url,
+      })
+        .then(() => {
+          res();
+        })
+        .catch((err) => {
+          rej(err.errors[0].message);
+        });
+    });
+  });
+};
+const editSet = (setNum, setData) => {
+  console.log(setData);
+  console.log(setNum);
+  return new Promise((res, rej) => {
+    sequelize.sync().then(() => {
+      console.log('setnum: ', setNum);
+      Set.update(
+        {
+          // set_num: setData.set_num,
+          name: setData.name,
+          year: setData.year,
+          num_parts: setData.num_parts,
+          theme_id: setData.theme_id,
+          img_url: setData.img_url,
+        },
+        {
+          where: { set_num: setNum },
+        }
+      )
+        .then(() => {
+          res();
+        })
+        .catch((err) => {
+          rej(err.errors[0].message);
+        });
+    });
+  });
+};
+const deleteSet = (setNum) => {
+  return new Promise((res, rej) => {
+    sequelize.sync().then(() => {
+      Set.destroy({
+        where: { set_num: setNum },
+      })
+        .then(() => {
+          res();
+        })
+        .catch((err) => {
+          rej(err.errors[0].message);
+        });
+    });
   });
 };
 module.exports = {
@@ -53,8 +187,8 @@ module.exports = {
   getAllSets,
   getSetByNum,
   getSetsByTheme,
+  getAllThemes,
+  addSet,
+  editSet,
+  deleteSet,
 };
-// initialize();
-// console.log(getAllSets());
-// console.log(getSetByNum('00-1'));
-// console.log(getSetsByTheme('Technic'));
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
- * WEB322 – Assignment 04
+ * WEB322 – Assignment 05
  *
  * I declare that this assignment is my own work in accordance with Seneca's
  * Academic Integrity Policy:
  *
  * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html*
  *
- * Name: Marcus Brown Student ID: 127900223 Date: 2023-11-12
+ * Name: Marcus Brown Student ID: 127900223 Date: 2023-11-25
  *
  * Published URL: https://ruby-wide-eyed-goshawk.cyclic.app/
  *
@@ -29,6 +29,74 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+app.get(
+  '/lego/addSet',
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    try {
+      const themeData = await legoData.getAllThemes();
+      res.render('addSet', { themes: themeData });
+    } catch (err) {
+      res.render('500', { message: 'Database error' });
+    }
+  }
+);
+app.post(
+  '/lego/addSet',
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    try {
+      await legoData.addSet(req.body);
+      res.redirect('/lego/sets');
+    } catch (err) {
+      res.render('500', {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      });
+    }
+  }
+);
+app.get(
+  '/lego/editSet/:num',
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    try {
+      const setData = await legoData.getSetByNum(req.params.num);
+      const themeData = await legoData.getAllThemes();
+      res.render('editSet', { themes: themeData, set: setData });
+    } catch (err) {
+      res.status(404).render('404', { message: err });
+    }
+  }
+);
+app.post(
+  '/lego/editSet',
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    console.log(req.body);
+    try {
+      await legoData.editSet(req.body.set_num, req.body);
+      res.redirect('/lego/sets');
+    } catch (err) {
+      res.render('500', {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      });
+    }
+  }
+);
+app.get(
+  '/lego/deleteSet/:num',
+  express.urlencoded({ extended: true }),
+  async (req, res) => {
+    try {
+      await legoData.deleteSet(req.params.num);
+      res.redirect('/lego/sets');
+    } catch (err) {
+      res.render('500', {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      });
+    }
+  }
+);
 app.get('/lego/sets', async (req, res) => {
   try {
     if (req.query.theme) {
@@ -67,3 +135,79 @@ async function startServer() {
   });
 }
 startServer();
+
+// const Sequelize = require('sequelize');
+
+// // set up sequelize to point to our postgres database
+// const sequelize = new Sequelize('LegoDB', 'MarcusB71', 'ah7p4KlCQWRb', {
+//   host: 'ep-royal-breeze-51150742-pooler.us-east-2.aws.neon.tech',
+//   dialect: 'postgres',
+//   port: 5432,
+//   dialectOptions: {
+//     ssl: { rejectUnauthorized: false },
+//   },
+// });
+
+// // Define a "Project" model
+// const Project = sequelize.define(
+//   'Project',
+//   {
+//     project_id: {
+//       type: Sequelize.INTEGER,
+//       primaryKey: true, // use "project_id" as a primary key
+//       autoIncrement: true, // automatically increment the value
+//     },
+//     title: Sequelize.STRING,
+//     description: Sequelize.TEXT,
+//   },
+//   {
+//     createdAt: false, // disable createdAt
+//     updatedAt: false, // disable updatedAt
+//   }
+// );
+// Define our "User" and "Task" models
+
+// const User = sequelize.define('User', {
+//   fullName: Sequelize.STRING, // the user's full name (ie: "Jason Bourne")
+//   title: Sequelize.STRING, // the user's title within the project (ie, developer)
+// });
+
+// const Task = sequelize.define('Task', {
+//   title: Sequelize.STRING, // title of the task
+//   description: Sequelize.TEXT, // main text for the task
+// });
+
+// // Associate Tasks with user & automatically create a foreign key
+// // relationship on "Task" via an automatically generated "UserId" field
+
+// User.hasMany(Task);
+// // synchronize the Database with our models and automatically add the
+// // table if it does not exist
+
+// sequelize.sync().then(() => {
+//   // Create user "Jason Bourne"
+//   User.create({
+//     fullName: 'Jason Bourne',
+//     title: 'developer',
+//   }).then((user) => {
+//     console.log('user created');
+
+//     // Create "Task 1" for the new user
+//     Task.create({
+//       title: 'Task 1',
+//       description: 'Task 1 description',
+//       UserId: user.id, // set the correct Userid foreign key
+//     }).then(() => {
+//       console.log('Task 1 created');
+//     });
+
+//     // Create "Task 2" for the new user
+//     Task.create({
+//       title: 'Task 2',
+//       description: 'Task 2 description',
+//       UserId: user.id, // set the correct Userid foreign key
+//     }).then(() => {
+//       console.log('Task 2 created');
+//     });
+//   });
+// });
